78cd0520ace8e5232303631e30ae6460cc70e4f1
TLQDung <tranlequocdung0@gmail.com>
2025-11-10 19:04:38 +0700

    Add activity player detail

---
+++ app/src/main/java/com/example/duan1/PlayerDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.duan1;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.duan1.databinding.ActivityPlayerDetailBinding;
+
+public class PlayerDetailActivity extends AppCompatActivity {
+
+}
